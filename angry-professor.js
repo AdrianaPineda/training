f61b9213d95819b9b5ef807c7485e79e51febb75
Adriana Pineda <apineda@twilio.com>
2019-06-02 18:19:22 +0300

    Update angry professor

--- angry-professor.js
+++ angry-professor.js
@@ -6,7 +6,7 @@ function angryProfessor(k, a) {
     //     }
     // })
     // return studentsArrivingOnTime >= k ? "NO" : "YES"
-    const reducer = (accumulator, current) => (current > 0) ? accumulator : accumulator + 1
+    const reducer = (accumulator, current) => (current <= 0) ? accumulator + 1 : accumulator
     const studentsArrivingOnTime = a.reduce(reducer, 0)
     return studentsArrivingOnTime >= k ? "NO" : "YES"
 }
