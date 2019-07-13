0162786a01161d9818b3fff1a418ab703eaf0cd6
Adriana Pineda <apineda@twilio.com>
2019-07-13 11:24:05 +0300

    Format file

--- grading-students.js
+++ grading-students.js
@@ -1,4 +1,3 @@
-
 function gradingStudents(grades) {
     /*
      * Write your code here.
@@ -6,9 +5,13 @@ function gradingStudents(grades) {
 
     let roundedGrades = []
     for (let index = 0; index < grades.length; index++) {
-      const grade = grades[index]
-      const roundedGrade = roundGrade(grade, multiple, minGradeValidForRounding)
-      roundedGrades.push(roundedGrade)
+        const grade = grades[index]
+        const roundedGrade = roundGrade(
+            grade,
+            multiple,
+            minGradeValidForRounding
+        )
+        roundedGrades.push(roundedGrade)
     }
 
     return roundedGrades
@@ -19,28 +22,28 @@ const multiple = 5
 
 function roundGrade(grade, multiple, minGradeValidForRounding) {
     if (grade < minGradeValidForRounding) {
-      return grade
+        return grade
     }
-    
+
     return roundGradeToNearestMultiple(grade, multiple)
 }
 
 function roundGradeToNearestMultiple(grade, multiple) {
-  const nearestMultipleValue = findNextMultiple(grade, multiple)
-  const difference = nearestMultipleValue - grade
-  if (difference < 3) {
-      return nearestMultipleValue
-  } else {
-      return grade
-  }
-} 
+    const nearestMultipleValue = findNextMultiple(grade, multiple)
+    const difference = nearestMultipleValue - grade
+    if (difference < 3) {
+        return nearestMultipleValue
+    } else {
+        return grade
+    }
+}
 
 function findNextMultiple(number, multiple) {
-  const divisionRoundedUp = Math.ceil(number / multiple)
-  return divisionRoundedUp*multiple
+    const divisionRoundedUp = Math.ceil(number / multiple)
+    return divisionRoundedUp * multiple
 }
 
 const result = gradingStudents([73, 67, 38, 33])
 
-console.log('******')
+console.log("******")
 console.log(result)
