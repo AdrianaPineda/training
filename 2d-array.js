17b15adc12a0a79762964843c9d1fc85732903b4
Adriana Pineda <apineda@twilio.com>
2019-08-23 07:12:14 +0300

    Create 2d-array.js

---
+++ 2d-array.js
@@ -0,0 +1,41 @@
+function hourglassSum(arr) {
+  let highestSum = -Infinity;
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const currentSum =
+        arr[i][j] +
+        arr[i][j + 1] +
+        arr[i][j + 2] +
+        arr[i + 1][j + 1] +
+        arr[i + 2][j] +
+        arr[i + 2][j + 1] +
+        arr[i + 2][j + 2];
+      if (currentSum > highestSum) {
+        highestSum = currentSum;
+      }
+    }
+  }
+  return highestSum;
+}
+
+// console.log(
+//   hourglassSum([
+//     [1, 1, 1, 0, 0, 0],
+//     [0, 1, 0, 0, 0, 0],
+//     [1, 1, 1, 0, 0, 0],
+//     [0, 0, 2, 4, 4, 0],
+//     [0, 0, 0, 2, 0, 0],
+//     [0, 0, 1, 2, 4, 0]
+//   ])
+// );
+
+console.log(
+  hourglassSum([
+    [0, -4, -6, 0, -7, -6],
+    [-1, -2, -6, -8, -3, -1],
+    [-8, -4, -2, -8, -8, -6],
+    [-3, -1, -2, -5, -7, -4],
+    [-3, -5, -3, -6, -6, -6],
+    [-3, -6, 0, -8, -6, -7]
+  ])
+);
