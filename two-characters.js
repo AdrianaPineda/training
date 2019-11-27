f9baa17f054eae286c726653f2025437ccc9a652
Adriana Pineda <apineda@twilio.com>
2019-11-27 21:04:40 +0200

    Refactor code

--- two-characters.js
+++ two-characters.js
@@ -1,54 +1,42 @@
 // Complete the alternate function below.
-function alternate(s) {
-    const stringAsArray = s.split("");
-    const dict = buildDict(stringAsArray);
-    let largestString = 0;
-    Object.keys(dict).forEach(key => {
-        Object.keys(dict).forEach(secondKey => {
-            if (key === secondKey) {
+//https://www.hackerrank.com/challenges/two-characters/problem
+function alternate(string) {
+    const chars = string.split("");
+    const charsSet = new Set(chars);
+    let largestSequence = 0;
+    charsSet.forEach(char => {
+        charsSet.forEach(secondChar => {
+            if (char === secondChar) {
                 return;
             }
-            const result = buildLongest(key, secondKey, stringAsArray);
-            if (result.length > largestString) {
-                largestString = result.length;
+            const sequence = buildLongestSequenceWithChars(char, secondChar, chars);
+            if (sequence.length > largestSequence) {
+                largestSequence = sequence.length;
             }
         });
     });
 
-    return largestString;
+    return largestSequence;
 }
 
-function buildDict(s) {
-    const dict = {};
-    s.forEach((element, index) => {
-        const ocurrences = dict[element] ? dict[element] : [];
-        ocurrences.push(index);
-        dict[element] = ocurrences;
-    });
-    console.log(dict);
-    return dict;
-}
-
-function buildLongest(key1, key2, originalStringArray) {
-    const result = [];
-    originalStringArray.forEach(currentKey => {
-        if (currentKey === key1 || currentKey === key2) {
-            result.push(currentKey);
+function buildLongestSequenceWithChars(char1, char2, originalChars) {
+    const charSequence = [];
+    originalChars.forEach(currentChar => {
+        if (currentChar === char1 || currentChar === char2) {
+            charSequence.push(currentChar);
         }
     });
 
-    const valid = isValidString(result);
-    return valid ? result : [];
+    const valid = isValidCharSequence(charSequence);
+    return valid ? charSequence : [];
 }
 
-function isValidString(resultingOcurrences) {
+function isValidCharSequence(charSequence) {
     let isValidString = true;
-    for (let i = 0; i < resultingOcurrences.length; i++) {
-        const currentElement = resultingOcurrences[i];
-        if (i > 0) {
-            if (currentElement === resultingOcurrences[i - 1]) {
-                isValidString = false;
-            }
+    for (let i = 1; i < charSequence.length; i++) {
+        const currentElement = charSequence[i];
+        if (currentElement === charSequence[i - 1]) {
+            isValidString = false;
         }
     }
 
