eb9c3647bc442fe1bc7a47e462dc15acbf679bee
Adriana Pineda <apineda@twilio.com>
2020-03-09 21:06:07 +0200

    Rename variable

--- codeforces/1200/vladik_and_flights.py
+++ codeforces/1200/vladik_and_flights.py
@@ -8,13 +8,14 @@
 # Problem
 
 
-def minimum_cost_between_airports(vladiks_house, olympiad, airports):
-    return 1 if airports[vladiks_house] != airports[olympiad] else 0
+def minimum_cost_between_airports(vladiks_house, olympiad, airports_companies):
+    return 1 if airports_companies[vladiks_house] != airports_companies[olympiad] else 0
 
 
 # Read input
 first_line = input().split()
 _, vladiks_house, olympiad = (int(i) for i in first_line)
 
-airports = list(input())
-print(minimum_cost_between_airports(vladiks_house - 1, olympiad - 1, airports))
+airports_companies = list(input())
+print(minimum_cost_between_airports(
+    vladiks_house - 1, olympiad - 1, airports_companies))
