8db9d13c20b74ed206897c1c544222de021a4cd1
Adriana Pineda <apineda@twilio.com>
2020-04-01 21:13:23 +0300

    Refactor exercise

--- codeforces/1300/game_of_credit_cards.py
+++ codeforces/1300/game_of_credit_cards.py
@@ -15,29 +15,70 @@ def value_index_greater_or_equal_than(ordered_list, number):
     return -1
 
 
-def flicks(sherlocks_card, sorted_moriarty_card, prefer_flicks):
-    moriartys_flicks = 0
-    sherlocks_flicks = 0
+# def flicks(sherlocks_card, sorted_moriarty_card, prefer_greater_digit):
+#     moriartys_flicks = 0
+#     sherlocks_flicks = 0
 
+#     for sherlocks_digit in sherlocks_card:
+#         increment = 1 if prefer_greater_digit else 0
+#         index = value_index_greater_or_equal_than(
+#             sorted_moriarty_card, sherlocks_digit + increment)
+#         index = index if index >= 0 else 0
+#         moriartys_digit = sorted_moriarty_card[index]
+#         if (moriartys_digit > sherlocks_digit):
+#             sherlocks_flicks += 1
+#         elif (moriartys_digit < sherlocks_digit):
+#             moriartys_flicks += 1
+#         sorted_moriarty_card.pop(index)
+#     return moriartys_flicks, sherlocks_flicks
+
+
+# def minimum_moriartys_flicks(sherlocks_card, sorted_moriarty_card):
+#     moriartys_flicks, _ = flicks(sherlocks_card, sorted_moriarty_card, False)
+#     return moriartys_flicks
+
+
+# def maximum_sherlocks_flicks(sherlocks_card, sorted_moriarty_card):
+#     _, sherlocks_flicks = flicks(sherlocks_card, sorted_moriarty_card, True)
+#     return sherlocks_flicks
+
+
+def minimum_moriartys_flicks(sherlocks_card, sorted_moriarty_card):
+    moriartys_flicks = 0
     for digit in sherlocks_card:
-        increment = 1 if prefer_flicks else 0
-        minimum_index = value_index_greater_or_equal_than(
-            sorted_moriarty_card, digit + increment)
-        if (minimum_index >= 0):
-            sherlocks_flicks += 1
-            sorted_moriarty_card.pop(minimum_index)
+        # guarantees minimum possible number of flicks Moriarty will get
+        index = value_index_greater_or_equal_than(
+            sorted_moriarty_card, digit)
+        if (index >= 0):
+            sorted_moriarty_card.pop(index)
         else:
             moriartys_flicks += 1
             sorted_moriarty_card.pop(0)
-    return moriartys_flicks, sherlocks_flicks
+    return moriartys_flicks
+
+
+def maximum_sherlocks_flicks(sherlocks_card, sorted_moriarty_card):
+    sherlocks_flicks = 0
+    for digit in sherlocks_card:
+        # guarantees maximum possible number of flicks that Sherlock can get from Moriarty
+        index = value_index_greater_or_equal_than(
+            sorted_moriarty_card, digit + 1)
+        if (index >= 0):
+            sherlocks_flicks += 1
+            sorted_moriarty_card.pop(index)
+        else:
+            sorted_moriarty_card.pop(0)
+    return sherlocks_flicks
 
 
 def solve(number_of_digits, sherlocks_card, moriartys_card):
     moriartys_card.sort()
-    moriartys_flicks, _ = flicks(sherlocks_card, moriartys_card.copy(), False)
-    _, sherlocks_flicks = flicks(sherlocks_card, moriartys_card.copy(), True)
+    min_moriartys_flicks = minimum_moriartys_flicks(
+        sherlocks_card, moriartys_card.copy())
+    max_sherlocks_flicks = maximum_sherlocks_flicks(
+        sherlocks_card, moriartys_card.copy())
 
-    return moriartys_flicks, sherlocks_flicks
+    return min_moriartys_flicks, max_sherlocks_flicks
 
 
 # Read input
