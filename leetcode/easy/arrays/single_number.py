9e199030cd2bdd64bb83840f342c39b82cc90c39
Adriana Pineda <apineda@twilio.com>
2020-04-18 11:25:28 +0300

    Update exercise

--- leetcode/easy/arrays/single_number.py
+++ leetcode/easy/arrays/single_number.py
@@ -1,8 +1,8 @@
 # https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/549/
 # Problem
 # Big O:
-# Time complexity:
-# Space complexity:
+# Time complexity: O(n*log(n)) + O(n) => O(n)
+# Space complexity: O(1)
 
 # Problem
 
@@ -18,17 +18,19 @@ class Solution(object):
         previous_number_occurrences = 1
         for i in range(1, len(nums)):
             current_number = nums[i]
+
             if previous_number == current_number:
                 previous_number_occurrences += 1
+                continue
+
+            if previous_number_occurrences == 1:
+                break
             else:
-                if previous_number_occurrences == 1:
-                    break
-                else:
-                    previous_number_occurrences = 1
-                    previous_number = current_number
+                previous_number_occurrences = 1
+                previous_number = current_number
 
         return previous_number
 
 
 solution = Solution()
-print(solution.singleNumber([1, 1, 2, 2, 3, 4, 4, 5, 5, 5]))
+print(solution.singleNumber([1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 5, 6]))
