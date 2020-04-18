4493d041820ce2c53cf57ec656d7a5221e6f60b4
Adriana Pineda <apineda@twilio.com>
2020-04-18 11:17:32 +0300

    Add single number exercise

---
+++ leetcode/easy/arrays/single_number.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/549/
+# Problem
+# Big O:
+# Time complexity:
+# Space complexity:
+
+# Problem
+
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        previous_number = nums[0]
+        previous_number_occurrences = 1
+        for i in range(1, len(nums)):
+            current_number = nums[i]
+            if previous_number == current_number:
+                previous_number_occurrences += 1
+            else:
+                if previous_number_occurrences == 1:
+                    break
+                else:
+                    previous_number_occurrences = 1
+                    previous_number = current_number
+
+        return previous_number
+
+
+solution = Solution()
+print(solution.singleNumber([1, 1, 2, 2, 3, 4, 4, 5, 5, 5]))
