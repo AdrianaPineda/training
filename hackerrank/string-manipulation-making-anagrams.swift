6a3048efa1c54bf6afdc167758f14a4f9f19c4a3
Adriana Pineda <apineda@twilio.com>
2020-08-17 18:33:06 +0300

    Update challenge

--- hackerrank/string-manipulation-making-anagrams.swift
+++ hackerrank/string-manipulation-making-anagrams.swift
@@ -8,6 +8,52 @@
 // Complete the makeAnagram function below.
 func makeAnagram(a: String, b: String) -> Int {
 
+    // Build "a" string chars occurrences dictionary
+    let aCharOcurrencesDict = getCharOcurrencesDict(string: a)
+
+    // Get chars in "b" that match chars in "a"
+    let charsToKeepFromB = getChars(fromString: b, thatAppearInDict: aCharOcurrencesDict)
+
+    let aCharCount = a.count // This line might cost O(a) time and it will most likely take more time, but the complexity won't change.
+    let bCharCount = b.count // This line might cost O(a) time and it will most likely take more time, but the complexity won't change.
+    let charsToRemoveFromB = bCharCount - charsToKeepFromB
+    let charsToRemoveFromA = (aCharCount - charsToKeepFromB)
+    let totalCharsToRemove = charsToRemoveFromA + charsToRemoveFromB
+
+    return totalCharsToRemove
+
+}
+
+func getCharOcurrencesDict(string: String) -> [Character: Int] {
+    var charDict:[Character: Int] = [:]
+    for char in string {
+        let currentOcurrence = charDict[char] ?? 0
+        charDict[char] = currentOcurrence + 1
+    }
+
+    return charDict
+}
+
+// This method loops over "string" chars and returns the number of chars that appear in the "dict" param
+func getChars(fromString string: String, thatAppearInDict dict:[Character: Int]) -> Int {
+    var charsThatMatch = 0
+    var dictToCompare = dict
+    for char in string {
+        if let count = dictToCompare[char], count > 0 {
+            dictToCompare[char] = (count - 1)
+            charsThatMatch += 1
+        }
+    }
+
+    return charsThatMatch
+}
+
+print(makeAnagram(a: "cde", b: "dcf"))
+print(makeAnagram(a: "cde", b: "abc"))
+print(makeAnagram(a: "hello", b: "hithere"))
+
+func makeAnagram_v1(a: String, b: String) -> Int {
+
     // Build a chars occurrences dictionary
     var aCharCount = 0
     var aDict:[Character: Int] = [:]
@@ -35,41 +81,3 @@ func makeAnagram(a: String, b: String) -> Int {
     return totalCharsToRemove
 
 }
-
-func makeAnagram_v2(a: String, b: String) -> Int {
-
-    // Build a chars occurrences dictionary
-    var aDict = getCharOcurrencesDict(string: a)
-
-    // Loop over b chars
-    var charsToRemoveFromB = 0
-    var charsToKeepFromB = 0
-    for char in b {
-        if let count = aDict[char], count > 0 {
-            aDict[char] = (count - 1)
-            charsToKeepFromB += 1
-        } else {
-            charsToRemoveFromB += 1
-        }
-    }
-
-    let aCharCount = a.count // This line might cost O(a) time, complexity won't change but it will most likely take more time. Hence the "makeAnagram" method above is preferred
-    let charsToRemoveFromA = (aCharCount - charsToKeepFromB)
-    let totalCharsToRemove = charsToRemoveFromA + charsToRemoveFromB
-
-    return totalCharsToRemove
-
-}
-
-func getCharOcurrencesDict(string: String) -> [Character: Int] {
-    var charDict:[Character: Int] = [:]
-    for char in string {
-        let currentOcurrence = charDict[char] ?? 0
-        charDict[char] = currentOcurrence + 1
-    }
-
-    return charDict
-}
-
-print(makeAnagram(a: "cde", b: "dcf"))
-print(makeAnagram(a: "hello", b: "hithere"))
