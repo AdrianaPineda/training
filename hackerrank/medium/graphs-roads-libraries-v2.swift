5fc2fe4e78ed579b0ddd9bf4f716edbaa0292a2f
Adriana Pineda <apineda@twilio.com>
2020-09-06 23:39:14 +0300

    Update code

--- hackerrank/medium/graphs-roads-libraries-v2.swift
+++ hackerrank/medium/graphs-roads-libraries-v2.swift
@@ -1,90 +1,56 @@
 // https://www.hackerrank.com/challenges/torque-and-development/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=graphs
 // Big O:
-// Time complexity: O(n^2)
+// Time complexity: O(n)
 // Space complexity: O(n)
 // Complete the roadsAndLibraries function below.
-typealias LandGraph = [Int: [Int]]
-typealias CitiesCost = [Int: Int]
-
-struct RepairCost {
-    var previous: Int
-    var updated: Int
-}
-
-struct CityInfo {
-    var city: Int
-    var citiesCost: CitiesCost
-    var landGraph: LandGraph
-    var libraryCost: Int
-    var roadCost: Int
+struct LandInfo {
+    var roads: Int
+    var startPoints: Int
 }
 
 func roadsAndLibraries(n: Int, c_lib: Int, c_road: Int, cities: [[Int]]) -> Int {
     if c_lib < c_road { // If library cost is cheaper, we should build libraries in each city
         return n * c_lib
     }
-    let graph = getLandGraph(cities: cities) // O(n)
-    return getRepairCost(totalCities: n, libraryCost: c_lib, roadCost: c_road, landGraph: graph)
+
+    let accessibleCities = getAccessibleCities(cities: cities) // O(n)
+    let landInfo = getLandInfo(totalCities: n, accessibleCities: accessibleCities) // O(n)
+    return landInfo.roads * c_road + landInfo.startPoints * c_lib
 }
 
-func getRepairCost(totalCities: Int, libraryCost: Int, roadCost: Int, landGraph: LandGraph) -> Int {
-    var citiesCost = [Int: Int]()
-    var cost = 0
+func getLandInfo(totalCities: Int, accessibleCities: [Int: Int]) -> LandInfo {
+    var roads = 0
+    var startPoints = 0
     var currentCity = 1
 
     while currentCity <= totalCities { // O(n)
 
-        if citiesCost[currentCity] == nil { // If there are no smaller nodes reaching the current city, a new library needs to be built
-            citiesCost[currentCity] = libraryCost
-            cost += libraryCost
+        if let _ = accessibleCities[currentCity] {
+            roads += 1
+        } else {
+            startPoints += 1 // If the city is not accessible, a new library needs to be built
+
         }
-
-        let cityInfo = CityInfo(city: currentCity, citiesCost: citiesCost, landGraph: landGraph, libraryCost: libraryCost, roadCost: roadCost)
-        let (cityCost, citiesCostUpdated) = getCityNodesRepairCost(info: cityInfo)
-
-        cost += cityCost
-        citiesCost = citiesCostUpdated
 
         currentCity += 1
     }
 
-    return cost
+    return LandInfo(roads: roads, startPoints: startPoints)
 }
 
-func getCityNodesRepairCost(info: CityInfo) -> (Int, CitiesCost) {
-    var citiesCostUpdated = info.citiesCost
-    var currentCost = 0
-
-    guard let nodes = info.landGraph[info.city] else {
-        return (currentCost, citiesCostUpdated)
-    }
-
-    for node in nodes { // O(n-1)
-        guard let _ = citiesCostUpdated[node] else { // If the node is reachable by another city, we ignore it
-            let cost = min(info.libraryCost, info.roadCost)
-            citiesCostUpdated[node] = cost
-            currentCost += cost
-            continue
-        }
-    }
-
-    return (currentCost, citiesCostUpdated)
-}
-
-func getLandGraph(cities: [[Int]]) -> LandGraph {
-    var graph = [Int: [Int]]()
-
+func getAccessibleCities(cities: [[Int]]) -> [Int: Int] {
+    var accessibleCities = [Int: Int]()
     for citiesPair in cities {
         let firstCity = citiesPair[0]
         let secondCity = citiesPair[1]
-        let smallerCity = min(firstCity, secondCity)
         let greaterCity = max(firstCity, secondCity)
-        var currentConnectedNodes = graph[smallerCity] ?? []
-        currentConnectedNodes.append(greaterCity)
-        graph[smallerCity] = currentConnectedNodes
+        accessibleCities[greaterCity] = 1
     }
-    return graph
+
+    return accessibleCities
 }
 
-print(roadsAndLibraries(n: 3, c_lib: 2, c_road: 1, cities: [[1, 2], [3, 1], [2, 3]]))
-print(roadsAndLibraries(n: 6, c_lib: 2, c_road: 5, cities: [[1, 3], [3, 4], [2, 4], [1, 2], [2, 3], [5, 6]]))
+
+
+print(roadsAndLibraries(n: 3, c_lib: 2, c_road: 1, cities: [[1, 2], [3, 1], [2, 3]])) // 4
+print(roadsAndLibraries(n: 6, c_lib: 2, c_road: 5, cities: [[1, 3], [3, 4], [2, 4], [1, 2], [2, 3], [5, 6]])) // 12
