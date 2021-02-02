9e1562e012cd38ffc9b76a9f16a587de242c0e95
Adriana Pineda <am.pineda206@gmail.com>
2021-02-01 20:58:48 -0500

    Add leetcode link for maximum depth binary tree

--- leetcode/easy/trees/maximum-depth-binary-tree.swift
+++ leetcode/easy/trees/maximum-depth-binary-tree.swift
@@ -1,3 +1,4 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/555/
 // Given the root of a binary tree, return its maximum depth.
 // A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
