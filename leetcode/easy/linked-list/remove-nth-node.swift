71917cb1bf0e63e4547ab69570dad439c56b53fd
Adriana Pineda <am.pineda206@gmail.com>
2021-02-05 20:12:13 -0500

    Add time complexity to exercise

--- leetcode/easy/linked-list/remove-nth-node.swift
+++ leetcode/easy/linked-list/remove-nth-node.swift
@@ -1,7 +1,7 @@
 // Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
-// Time complexity: O()
-// Space complexity: O()
+// Time complexity: O(n)
+// Space complexity: O(1)
 
 public class ListNode {
     public var val: Int
