1efc4d960d56574318e9c1551053af46fb92be07
AdrianaPineda <am.pineda206@gmail.com>
2022-02-02 20:32:49 -0500

    [medium-arrays] Update solution of longest substring without repeating chars exercise from leetcode

--- leetcode/2-medium/arrays-strings/longest-substring-no-repeat-chars-v2.swift
+++ leetcode/2-medium/arrays-strings/longest-substring-no-repeat-chars-v2.swift
@@ -1,6 +1,53 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
 // Given a string s, find the length of the longest substring without repeating characters.
 
+// Time complexity: O(n)
+// Space complexity: O(n)
+func lengthOfLongestSubstring(_ s: String) -> Int {
+    let charArray = Array(s)
+    var charsIndexes = [Character: Int]()
+    var maxLength = 0
+
+    var i = 0
+    for (j, char) in charArray.enumerated() {
+        if let charIndex = charsIndexes[char] {
+            i = max(i, charIndex + 1)
+        }
+
+        maxLength = max(maxLength, j - i + 1)
+        charsIndexes[char] = j
+    }
+
+    return maxLength
+}
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+func lengthOfLongestSubstring(_ s: String) -> Int {
+    let charArray = Array(s)
+    var charsCount = [Character: Int]()
+    var leftPointer = 0
+    var rightPointer = 0
+    var maxLength = 0
+
+    while rightPointer < charArray.count {
+
+        let charAtRight = charArray[rightPointer]
+        charsCount[charAtRight] = (charsCount[charAtRight] ?? 0) + 1
+
+        while let currCount = charsCount[charAtRight], currCount > 1 {
+            let charAtLeft = charArray[leftPointer]
+            charsCount[charAtLeft] = (charsCount[charAtLeft] ?? 0) - 1
+            leftPointer += 1
+        }
+
+        maxLength = max(maxLength, rightPointer - leftPointer + 1)
+        rightPointer += 1
+    }
+
+    return maxLength
+}
+
 // Time complexity: O(n^2)
 // Space complexity: O(n)
 func lengthOfLongestSubstring(_ s: String) -> Int {
